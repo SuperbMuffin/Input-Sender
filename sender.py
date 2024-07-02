0989758ac895769caa6c3008ba7fd89a1ee66049
Elliot <166047473+Elliot-Tester@users.noreply.github.com>
2024-07-02 15:08:35 +0200

    Update sender.py

--- sender.py
+++ sender.py
@@ -20,11 +20,7 @@ else:
     sys.stderr = open(os.devnull, 'w')
 
 
-# Checks If Set Up
-if not vars.setup
-    tk.messagebox("Run setup.py", "Fill In The Info")
-    os.system("python setup.py")
-    sys.exit()
+
 # Your bot token and voice channel ID
 BOT_TOKEN = vars.s_token
 VOICE_CHANNEL_ID = vars.vc_id
